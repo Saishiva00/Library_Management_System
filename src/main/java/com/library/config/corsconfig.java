1668e4b5c9b40f4094b974abea1530e6e94236e0
Akula Sai shiva <saishivaakula111@gmail.com>
2026-03-01 17:36:51 +0530

    corrected cors files

--- src/main/java/com/library/config/corsconfig.java
+++ src/main/java/com/library/config/corsconfig.java
@@ -1,30 +1,19 @@
 package com.library.config;
 
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class corsconfig {
+public class corsconfig implements WebMvcConfigurer {
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-
-        return new WebMvcConfigurer() {
-
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-
-                registry.addMapping("/**")
-                        .allowedOrigins(
-                                "https://library-management-system-saishiva.netlify.app"
-                        )
-                        .allowedMethods("*")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-
-            }
-        };
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins("http://localhost:8080",
+                        "http://localhost:3000",
+                        "https://library-management-system-saishiva.netlify.app/" )
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
     }
 }
