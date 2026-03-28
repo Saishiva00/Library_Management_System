078b4e0eea6062bdd161f4c163f50819d5f0b75d
Saishiva Akula <168960926+Saishiva00@users.noreply.github.com>
2026-03-28 23:57:12 +0530

    Update AuthController.java

--- src/main/java/com/library/controller/AuthController.java
+++ src/main/java/com/library/controller/AuthController.java
@@ -13,7 +13,7 @@ import java.util.Map;
 @RestController
 @RequestMapping("/api/auth")
 @RequiredArgsConstructor
-@CrossOrigin(origins = ${FRONTEND_URL})
+
 public class AuthController {
 
     private final AuthService authService;
